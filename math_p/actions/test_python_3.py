9119dcdff477ab011d06c734cb714a2569d5fc33
root <root@nautafstkstrm01.novalocal>
2022-06-13 07:46:27 +0000

    Addition of new files

---
+++ math_p/actions/test_python_3.py
@@ -0,0 +1,27 @@
+import json
+import sys
+import ast
+
+from st2common.runners.base_action import Action
+import paramiko
+class MyEchoAction(Action):
+	result = dict()
+	def run(self, server_name,user_name):
+		try:
+			ssh = paramiko.SSHClient()
+                        #ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+			ssh.load_system_host_keys()
+			key_file = paramiko.RSAKey.from_private_key_file("/home/automation/.ssh/id_rsa")
+			ssh.connect(server_name, username=user_name, pkey=key_file, allow_agent=False, look_for_keys=False)
+			TopProcessCommand='hostname'
+			stdin, stdout, stderr = ssh.exec_command(TopProcessCommand)
+			out=stdout.read()
+			err = stderr.read().decode("utf-8")
+			if(err == ''):
+				output = {"retCode" : "0", "result" : out, "retDesc" : "Success"}
+				print(output)
+			else:
+				raise Exception(err)
+		except Exception as err:
+			output = {"retCode" : "1", "result" : err, "retDesc" : "Failure"}
+			print(output)
